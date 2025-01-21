ab4d087eaa3c7bbe1100d1559b0f2cfe1df9a937
Ryan Williams <ryswilli@ucsc.edu>
2025-01-21 00:48:24 -0500

    finished module 3

--- concert/urls.py
+++ concert/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r"^$", views.index, name="index"),
-    path("", views.songs, name="songs"),
-    path("", views.photos, name="photos"),
-    path("", views.login_view, name="login"),
-    path("", views.logout_view, name="logout"),
-    path("", views.signup, name="signup"),
-    path("", views.concerts, name="concerts"),
-    path("concert-detail/<int:id>", views.concert_detail, name="concert_detail"),
-    path("concert_attendee/", views.concert_attendee, name="concert_attendee"),
+    re_path(r"^$", views.index, name="index"),  # For the homepage or default route
+    path("songs/", views.songs, name="songs"),  # Route for songs
+    path("photos/", views.photos, name="photos"),  # Route for photos
+    path("login/", views.login_view, name="login"),  # Route for login
+    path("logout/", views.logout_view, name="logout"),  # Route for logout
+    path("signup/", views.signup, name="signup"),  # Route for signup
+    path("concerts/", views.concerts, name="concerts"),  # Route for concerts
+    path("concert-detail/<int:id>/", views.concert_detail, name="concert_detail"),  # Route for concert detail
+    path("concert-attendee/", views.concert_attendee, name="concert_attendee"),  # Route for concert attendee
 ]
+
